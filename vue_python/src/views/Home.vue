8735ea0ccdc487afb3187d9911a7be4855d6cfcf
jaqkqh15 <jaqkqh15@naver.com>
2021-09-30 15:20:00 +0900

    [황정아] (210929) 99일차 복습

---
+++ vue_python/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-link :to="{ name: 'LoginTest' }" 
+      class="nav-link" 
+      active-class="active">LoginTest
+    </router-link>
+</template>
+
+<script>
+ 
+  export default {
+    name: 'Home',
+
+    components: {
+
+    },
+  }
+</script>
