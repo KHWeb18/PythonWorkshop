bd088a18f06e8b2ec0bed64f244eef1c5f04e0d9
jaqkqh15 <jaqkqh15@naver.com>
2021-10-05 15:24:25 +0900

    [황정아] (211004) 102일차 복습

---
+++ spring_python/src/main/java/com/example/spring_python/entity/Product.java
@@ -0,0 +1,14 @@
+package com.example.spring_python.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Product {
+    private Integer cost;
+}
